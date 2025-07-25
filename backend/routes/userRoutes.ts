577366372e4765e149cc844c83bb8842d71111c9
Tyler Thomas <131617288+tmoney762558@users.noreply.github.com>
2025-07-25 15:56:13 -0500

    Fixed error in userRoute where userData was provided within another object instead of directly (#4)

--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -73,9 +73,7 @@ router.get(
         friendRequestsSent: outgoingFR.rows,
       };
 
-      console.log(userData);
-
-      res.status(200).json({ userData });
+      res.status(200).json(userData);
     } catch (err) {
       console.error(err);
       res.status(500).json({ message: "Internal Server Error" });
